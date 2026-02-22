35dafa30c3fcaf484b07364f6143caa1028f0a49
Viktor <palchinskij19@gmail.com>
2026-02-22 13:53:44 +0300

    feat: add lesson 2 task 1 — structs and methods

---
+++ lesson_2/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main()  {
+	fmt.Println("Lesson 2")
+
+	task1 := NewTask("1", "Study Go lesson 1")
+	task2 := NewTask("2", "Plan traning")
+
+	fmt.Printf("is task 1 over %b", task1.IsCompleted())
+	task2.Complete()
+	fmt.Printf("is task 2 over %b", task2.IsCompleted())	
+}
+
+type Task struct {
+	ID string
+	Title string
+	Description string
+	Completed bool
+	CreatedAt time.Time
+}
+
+func NewTask(id, title string) *Task{
+	return &Task{
+		ID: id,
+		Title: title,
+		CreatedAt: time.Now(),
+	}
+}
+
+func (t *Task) Complete()  {
+	t.Completed = true
+}
+
+func (t *Task) IsCompleted() bool {
+	return t.Completed
+}
