e98107d910f5d11bfebd890bca9a7fedc2ca3917
Viktor <palchinskij19@gmail.com>
2026-02-22 12:25:45 +0300

    fix: check divisibility by 15 before 3 and 5 in fizzBuzz

--- lesson_1/main.go
+++ lesson_1/main.go
@@ -4,4 +4,20 @@ import "fmt"
 
 func main() {
 	fmt.Println("Pomodoro Timer v0.1")
+
+	for i := 1; i < 31; i++ {
+		fmt.Printf("number %d is %s\n", i, fizzBuzz(i))
+	}
+}
+
+func fizzBuzz(n int) string {
+	if n%15 == 0 {
+		return "FizzBuzz"
+	} else if n%3 == 0 {
+		return "Fizz"
+	} else if n%5 == 0 {
+		return "Buzz"
+	}
+
+	return ""
 }
