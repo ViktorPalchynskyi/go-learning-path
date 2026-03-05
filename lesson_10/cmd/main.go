bd626c67e44c88655f177f0208e14dff5ef6c885
Viktor <palchinskij19@gmail.com>
2026-03-05 06:47:00 +0300

    feat: add lesson 10 task 2 — processItems with context cancellation

--- lesson_10/cmd/main.go
+++ lesson_10/cmd/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"time"
+)
 
 func main() {
 	fmt.Println("Lesson 10")
+	items := []string{"a", "b", "c", "d", "e"}
+	ctx, cancel := context.WithTimeout(context.Background(), 500*time.Millisecond)
+	defer cancel()
+	err := processItems(ctx, items)
+	if err != nil {
+		fmt.Println("cancelled:", err)
+	}
+}
+
+
+func processItems(ctx context.Context, items []string) error{
+	for _, item := range items {
+		select{
+		case <- ctx.Done():
+			return ctx.Err()
+		default:
+			time.Sleep(200 * time.Millisecond)
+			fmt.Printf("Processing item: %v\n", item)
+		}
+	}
+
+	return nil
 }
